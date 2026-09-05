960d1d7cf6e895a7b9f59e88997404351c011bef
kertousr-hue <kertous.r@gmail.com>
2026-09-05 16:39:41 +0200

    Mettre en cache le module API

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE='mpc-studio-v1';
-const SHELL=['./','./index.html','./styles.css','./app.js','./supabase-config.js','./manifest.webmanifest'];
+const SHELL=['./','./index.html','./styles.css','./app.js','./api-services.js','./supabase-config.js','./manifest.webmanifest'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(SHELL)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch',e=>{
